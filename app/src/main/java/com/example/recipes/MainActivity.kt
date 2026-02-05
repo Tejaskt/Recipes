9a420732bd6e7d470c9999cb5c064c38bea571c3
tejaskt <tejasktgeetanjali88@gmail.com>
2026-02-05 11:54:05 +0530

    Default.

---
+++ app/src/main/java/com/example/recipes/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.recipes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.recipes.ui.theme.RecipesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RecipesTheme {
+
+            }
+        }
+    }
+}
